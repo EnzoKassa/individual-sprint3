e151f853a9c5ecd4e9f17bce7711c84dcafa772f
EnzoKassa <enzo.kassawara@sptech.school>
2024-06-03 10:11:18 -0300

    pesquisas sendo inseridas no banco de dados

--- web-data-viz/src/models/pesquisaModel.js
+++ web-data-viz/src/models/pesquisaModel.js
@@ -1,18 +1,20 @@
 var database = require("../database/config")
 
-function pesquisar()
-{
+function pesquisar() {
     var instrucaoSql = "SELECT p.avaliacao, p.satisfacao, u.nome from Pesquisa as p JOIN Usuario as u ON p.fkUsuario = u.idUsuario ";
 
     console.log(instrucaoSql)
     return database.executar(instrucaoSql)
 }
 
-function avaliacao(avaliacao , pesquisa , fkUsuario) {
+function avaliacao(avaliacao, pesquisa, fkUsuario) {
 
     var instrucaoSql = ` INSERT INTO Pesquisa (avaliacao, satisfacao, fkUsuario) VALUES ('${avaliacao}' , '${pesquisa}' , ${fkUsuario})`
-    
+    console.log(instrucaoSql)
+    return database.executar(instrucaoSql)
 }
 module.exports = {
-    pesquisar
+    pesquisar,
+    avaliacao,
+
 }
