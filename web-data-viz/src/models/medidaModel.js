d9327afbb13891d9169a06865ad791d5277ac2b9
EnzoKassa <enzo.kassawara@sptech.school>
2024-06-05 13:47:05 -0300

    ultimas mudanças

--- web-data-viz/src/models/medidaModel.js
+++ web-data-viz/src/models/medidaModel.js
@@ -2,7 +2,7 @@ var database = require("../database/config");
 
 function buscarUltimasMedidas() {
 
-    var instrucaoSql = `select count(satisfacao) as quantidade from pesquisa group by satisfacao;
+    var instrucaoSql = `select count(satisfacao) as quantidade from Pesquisa group by satisfacao;
     `;
 
     console.log("Executando a instrução SQL: \n" + instrucaoSql);
